ba6ba22b4fd0aeb3a5d1d071670ad414f27cd075
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-06-16 13:39:23 -0400

    readme updated, server.js added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
