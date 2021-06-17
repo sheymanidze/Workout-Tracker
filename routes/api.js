ec0cd288a93b5878a5b7eed35a28bb3bc3c143f6
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-06-17 14:52:31 -0400

    add exercise

--- routes/api.js
+++ routes/api.js
@@ -46,7 +46,7 @@ router.get("/api/workouts", (req, res) => {
 });
 
 
-//Create workout
+//Create a workout
 router.post("/api/workouts", (req, res) => {
   db.Workout.create(req.body)
     .then((newWorkout) => {
@@ -58,7 +58,23 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
-
+// Add an exercise to a workout
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    {
+      new: true
+    }
+  )
+    .then((newExercise) => {
+      res.json(newExercise)
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+      console.log(err);
+    })
+});
 
 
 module.exports = router;
