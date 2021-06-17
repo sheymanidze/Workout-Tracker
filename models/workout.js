40fa11d19d48c7e61dd35c0a4949373221d49eee
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-06-17 13:39:22 -0400

    oics added

--- models/workout.js
+++ models/workout.js
@@ -36,9 +36,7 @@ const workoutSchema = new Schema({
     }]
 });
 
-// workoutSchema.virtual("totalDuration").get(function () {
-//   return this.exercises.reduce((total, exercise) => total + exercise.duration, 0);
-// });
+
 
 const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
